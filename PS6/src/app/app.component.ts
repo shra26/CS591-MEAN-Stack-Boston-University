9cb50035acf80e0d0d26ce6a12f685677ed6d5e4
shra26 <tvsg26@gmail.com>
2019-06-24 10:32:21 -0400

    PS6 merged

--- PS6/src/app/app.component.ts
+++ PS6/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { WEATHER } from './models/weather';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { connection } from './config/constants';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PS6';
+Weather: WEATHER;
+  httpClient: any;
+  city: string;
+  requestData: any
+  headers = new HttpHeaders()
+    .set("Content-Type", "application/json");
+  checkCity() {
+    this.httpClient.get(connection.urlMain,{
+      headers: new HttpHeaders()
+      .set("data", this.city)
+    }).subscribe(data => {
+      this.Weather = data.weather;
+      this.requestData = data.req
+    });
+  }
+  constructor(private http: HttpClient) {
+    this.httpClient = http;
+  }
+  ngOnInit() {}
 }
