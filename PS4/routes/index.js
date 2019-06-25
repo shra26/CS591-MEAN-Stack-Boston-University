652b20abb8410de97baf8668352f43108be809fc
shra26 <tvsg26@gmail.com>
2019-06-25 11:01:26 -0400

    temp changes

--- PS4/routes/index.js
+++ PS4/routes/index.js
@@ -1,9 +1,36 @@
 let express = require('express');
 let router = express.Router();
+const db = require('../mongo/mongo');
 
+db.connect((err, client) => {
+	if (err) {
+		console.log(`ERR: ${err}`);
+	} else {
+		console.log(`Connected`);
+	}
+});
+router.post('/', (req, res, next) => {
+	let weather = req.body.name;
+	let weatherObj = {
+		weather
+	};
+	let mongo = db.getDB();
+	mongo.collection('WeatherDB').insertOne(req.body, (err, r) => {
+		res.send('success');
+	});
+});
+router.get('/:weather', (req, res, next) => {
+	let mongo = db.getDB();
+	mongo
+		.collection('WeatherDB')
+		.find({ weather: req.params.weather })
+		.toArray((err, docs) => {
+			console.log(docs);
+		});
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
