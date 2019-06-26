d0772f704b5728188e80c573702e172c874001fe
shra26 <tvsg26@gmail.com>
2019-06-26 12:33:33 -0400

    PS8 solved

--- PS4/routes/ps4.js
+++ PS4/routes/ps4.js
@@ -2,17 +2,69 @@ let express = require('express');
 let router = express.Router();
 let request = require('request');
 let con = require('../config/constants');
+const db = require('../mongo/mongo');
 /* GET users listing. */
 
-const resolver = new Promise((resolve, reject) => {
-	request(con.url1 + con.url2, (error, response, body) => {
-		if (error) reject(new Error(error));
-		else resolve(JSON.parse(body));
-	});
+// let resolver = new Promise((resolve, reject) => {
+// 	// request(con.url1 + con.url2, (error, response, body) => {
+// 	request(con.hitUrl21 + con.hitUrl22, (error, response, body) => {
+// 		if (error) reject(new Error(error));
+// 		else resolve(JSON.parse(body));
+// 	});
+// });
+db.connect((err, client) => {
+	if (err) {
+		console.log(`ERR: ${err}`);
+	} else {
+		console.log(`Connected`);
+	}
 });
+let fromDB = false;
 router.get('/', (req, res, next) => {
-	resolver.then(value => {
-		res.render('ps4', { weather: value.results.sunrise });
+	let mongo = db.getDB();
+	mongo.collection('WeatherDB').findOne({ city: req.headers.data || 'Boston' }, (err, result) => {
+		if (err) {
+			return next(err);
+		} else {
+			if (result == null) {
+				let resolver = new Promise((resolve, reject) => {
+					// request(con.url1 + con.url2, (error, response, body) => {
+					request(con.hitUrl21 + con.hitUrl22 + req.headers.data, (error, response, body) => {
+						if (error) reject(new Error(error));
+						else resolve(JSON.parse(body));
+					});
+				});
+				resolver
+					.then(val => {
+						return new Promise((resolve, reject) => {
+							request(
+								con.url1 +
+									'json?lat=' +
+									val[0].latt_long.split(',')[0] +
+									'&lng=' +
+									val[0].latt_long.split(',')[1] +
+									'&date=today',
+								(error, response, body) => {
+									if (error) reject(new Error(error));
+									else resolve(JSON.parse(body));
+								}
+							);
+						});
+					})
+					.then(val => {
+						fromDB = false;
+						mongo.collection('WeatherDB').insertOne({ city: req.headers.data, obj: val.results });
+						res.send({ weather: val.results, fromDB: fromDB });
+					});
+			} else {
+				fromDB = true;
+				console.log(`Result: ${result.obj}`);
+				res.send({
+					weather: result.obj,
+					fromDB: fromDB /*, req: result req.headers.data || 'Boston'*/
+				});
+			}
+		}
 	});
 });
 
--- PS8/src/app/app.component.ts
+++ PS8/src/app/app.component.ts
@@ -9,20 +9,22 @@ import { connection } from './config/constants';
 })
 export class AppComponent implements OnInit {
   title = 'PS6';
-Weather: WEATHER;
+  Weather: WEATHER;
   httpClient: any;
   city: string;
-  requestData: any
-  headers = new HttpHeaders()
-    .set("Content-Type", "application/json");
+  requestData: any;
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
   checkCity() {
-    this.httpClient.get(connection.urlMain,{
-      headers: new HttpHeaders()
-      .set("data", this.city)
-    }).subscribe(data => {
-      this.Weather = data.weather;
-      this.requestData = data.req
-    });
+    this.httpClient
+      .get(connection.urlMain, {
+        headers: new HttpHeaders()
+          .set('data', this.city || 'Boston')
+          .set('Access-Control-Allow-Origin', 'http://localhost:3000')
+      })
+      .subscribe(data => {
+        this.Weather = data.weather;
+        this.requestData = data.fromDB;
+      });
   }
   constructor(private http: HttpClient) {
     this.httpClient = http;
